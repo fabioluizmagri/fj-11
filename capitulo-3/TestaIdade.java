719f2a2de935bea93853c259169b5ff5c76d6176
Fabio Luiz Magri <fabioluizmagri@gmail.com>
2013-06-04 08:18:08 -0300

    Onde testar esses codigos? Testa idade - variaveis int

---
+++ capitulo-3/TestaIdade.java
@@ -0,0 +1,19 @@
+public class TestaIdade {
+	public static void main (String[] args) {
+		
+		// declara a idade
+		int idade;
+		idade = 15;
+		
+		// imprime a idade
+		System.out.println(idade);
+		
+		// gera a idade do ano seguinte
+		int idadeAnoQueVem;
+		idadeAnoQueVem = idade + 1;
+		
+		// imprime a idade
+		System.out.println(idadeAnoQueVem);
+	
+	}
+}
